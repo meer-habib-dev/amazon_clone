ef0c418aba02de810e1395899e3696d9ed7fa265
Meer habib <meerhabib@Meers-MacBook-Air.local>
2022-02-04 00:00:07 +0600

    updated

---
+++ src/pages/api/create-checkout-session.js
@@ -0,0 +1,34 @@
+        const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+        export default  async (req,res) => {
+            const {items, email} = req.body
+
+            const transfromedIems = items.map(item => ({
+        description: item.description,
+        quantity: 1,
+        price_data: {
+        currency: 'usd',
+        unit_amount: item.price * 100,
+        product_data:{
+            name:item.title,
+            images: [item.image]
+        }
+        }
+            }))
+            const session = await stripe.checkout.sessions.create({
+                payment_method_types:['card'],
+                shipping_rates: ['shr_1KP7aQGIfCQ9sRgSeAw0od2v'],
+                shipping_address_collection: {
+                    allowed_countries: ['BD','US','CA','GB']
+                },  
+                line_items: transfromedIems,
+                mode:'payment',
+                success_url: `${process.env.HOST}/success`,
+                cancel_url: `${process.env.HOST}/checkout`,
+                metadata: {
+                    email,
+                    images: JSON.stringify(items.map(item => item.images))
+                }
+            })
+            console.log(session.id,'sada');
+            res.status(200).json({id:session.id})
+        }
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -6,11 +6,31 @@ import Image from "next/image";
 import CheckOutProduct from "../components/CheckOutProduct";
 import Currency from "react-currency-formatter";
 import { useSession } from "next-auth/client";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
 
+const stripePromise =  loadStripe(process.env.stripe_public_key)
 const Checkout = () => {
   const items = useSelector(selectItems);
   const [session] = useSession();
-  const total = useSelector(selectTotal)
+  const total = useSelector(selectTotal);
+  const createCheckoutSession = async() => {
+    const stripe = await stripePromise
+    // create backend to create stripe session
+
+    const createSession = await axios.post('/api/create-checkout-session',{
+      items,
+      email: session.user.email
+    })
+
+    // redirect user/customer to checkout page
+    const result = await stripe.redirectToCheckout({
+      sessionId: createSession.data.id
+    })
+    if(result.error){
+      alert(result.error.message)
+    }
+  }
   return (
     <div className="bg-gray-100">
       <Header />
@@ -46,7 +66,7 @@ const Checkout = () => {
                  </span>
               
               </h2>
-              <button disabled={!session} className={`button mt-3 ${!session && 'from-gray-500 to-gray-300 border-gray-200 text-gray-300 cursor-not-allowed' }`}>{!session ? 'Sign in to checkout' : 'Proceed to checkout'}</button>
+              <button role='link' onClick={createCheckoutSession} disabled={!session} className={`button mt-3 ${!session && 'from-gray-500 to-gray-300 border-gray-200 text-gray-300 cursor-not-allowed' }`}>{!session ? 'Sign in to checkout' : 'Proceed to checkout'}</button>
               </>
             )
           }
